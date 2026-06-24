e588e834888fa405d5da89b711d9e6cb5c280640
alan araújo <ag23672311@gmail.com>
2026-06-24 17:59:34 -0300

    Calculadora de Gorjetas

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+"use strict";
+const inputValor = document.getElementById("valor");
+const botaoCalcular = document.getElementById("botao");
+const textoResultado = document.getElementById("resultado");
+function calcularGorjeta() {
+    const valorConta = parseFloat(inputValor.value);
+    if (isNaN(valorConta) || valorConta <= 0) {
+        textoResultado.textContent = "Por favor, insira um valor válido.";
+        return;
+    }
+    const gorjeta = valorConta * 0.10;
+    textoResultado.textContent = `A gorjeta de 10% é: R$ ${gorjeta.toFixed(2)}`;
+}
+botaoCalcular.addEventListener("Click", calcularGorjeta);
---
+++ src/script.ts
@@ -0,0 +1,17 @@
+const inputValor = document.getElementById("valor") as HTMLInputElement;
+const botaoCalcular = document.getElementById("botao") as HTMLButtonElement;
+const textoResultado = document.getElementById("resultado") as HTMLParagraphElement;
+
+function calcularGorjeta(): void {
+    const valorConta: number = parseFloat(inputValor.value);
+
+if (isNaN(valorConta) || valorConta <= 0) {
+textoResultado.textContent="Por favor, insira um valor válido.";
+return;
+}
+
+const gorjeta: number = valorConta * 0.10; 
+textoResultado.textContent = `A gorjeta de 10% é: R$ ${gorjeta.toFixed(2)}`;
+}
+
+botaoCalcular.addEventListener("Click", calcularGorjeta);
